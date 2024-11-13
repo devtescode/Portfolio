4e6f3871c644e2ada59c7a22c7d798236f9d5eb7
TESCODE <teslimagboola09@gmail.com>
2024-11-13 12:34:20 +0100

    solving vercel bug

--- src/components/AdminLogin/AdminLogin.jsx
+++ src/components/AdminLogin/AdminLogin.jsx
@@ -35,7 +35,7 @@ const AdminLogin = () => {
 
     return (
         <div>
-            <h2>Admin Login</h2>
+            <h2>Admin-Login</h2>
             <main className="flex-shrink-0">
                 <div>
                     <Narbar />
