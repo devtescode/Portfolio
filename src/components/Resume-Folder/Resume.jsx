e7ec19b397e1a67934371bca99af315b96772618
TESCODE <teslimagboola09@gmail.com>
2024-11-14 16:00:05 +0100

    remove console

--- src/components/Resume-Folder/Resume.jsx
+++ src/components/Resume-Folder/Resume.jsx
@@ -14,7 +14,7 @@ const Resume = () => {
                             <span className="text-gradient d-inline">Resume</span>
                         </h1>
                     </div>
-                    <div className="row gx-5 justify-content-center">
+                    <div className="row gx-2 justify-content-center ">
                         <div className="col-lg-11 col-xl-9 col-xxl-8">
                             {/* Experience Section*/}
                             <section>
