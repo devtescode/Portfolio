2f69827f0da02955cf5529d61fbc1ba71692aeb1
devtescode <teslimagboola09@gmail.com>
2025-07-24 14:05:11 +0100

    change the picture

--- src/components/Portfolio-page/Portfolio.jsx
+++ src/components/Portfolio-page/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Portfolioimage from '../../Images/portfolio.png'
+import Portfolioimage from '../../Images/secportfolio.png'
 import { useNavigate } from 'react-router-dom'
 import Narbar from '../Navbar-folder/Narbar'
 
@@ -101,6 +101,7 @@ const Portfolio = () => {
                                         <img
                                             className="profile-img"
                                             src={Portfolioimage}
+                                            style={{  height: "100%",   filter: "brightness(1.1)"  }}
                                             alt="..."
                                         />
                                         <div className="dots-1">
